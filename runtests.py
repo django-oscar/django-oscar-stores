e19beb8b0af10876fffcf1b5ac4849e500d71339
Sebastian Vetter <sebastian@roadside-developer.com>
2012-09-02 23:13:10 +1000

    adds configuration for Travis CI

---
+++ runtests.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+import os
+import sys
+import logging
+from optparse import OptionParser
+
+from django.conf import settings
+
+from oscar.defaults import OSCAR_SETTINGS
+from oscar import OSCAR_MAIN_TEMPLATE_DIR, OSCAR_CORE_APPS
+
+location = lambda x: os.path.join(os.path.dirname(os.path.realpath(__file__)), x)
+
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+            }
+        },
+        MEDIA_ROOT=location('public/media'),
+        MEDIA_URL='/media/',
+        STATIC_URL='/static/',
+        STATICFILES_DIRS=(location('static/'),),
+        STATIC_ROOT=location('public'),
+        STATICFILES_FINDERS=(
+            'django.contrib.staticfiles.finders.FileSystemFinder',
+            'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+        ),
+        TEMPLATE_LOADERS=(
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        ),
+        MIDDLEWARE_CLASSES=(
+            'django.middleware.common.CommonMiddleware',
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+            'debug_toolbar.middleware.DebugToolbarMiddleware',
+            'oscar.apps.basket.middleware.BasketMiddleware',
+        ),
+        ROOT_URLCONF='sandbox.sandbox.urls',
+        TEMPLATE_DIRS=(
+            location('templates'),
+            os.path.join(OSCAR_MAIN_TEMPLATE_DIR, 'templates'),
+            OSCAR_MAIN_TEMPLATE_DIR,
+        ),
+        INSTALLED_APPS=[
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.sites',
+            'django.contrib.messages',
+            'django.contrib.staticfiles',
+            'django.contrib.admin',
+        ] + OSCAR_CORE_APPS + [
+            'stores',
+        ],
+        AUTHENTICATION_BACKENDS=(
+            'oscar.apps.customer.auth_backends.Emailbackend',
+            'django.contrib.auth.backends.ModelBackend',
+        ),
+        LOGIN_REDIRECT_URL='/accounts/',
+        APPEND_SLASH=True,
+        HAYSTACK_CONNECTIONS={
+            'default': {
+                'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
+            },
+        },
+        NOSE_ARGS=['-s', '-x'],
+        **OSCAR_SETTINGS
+    )
+
+logging.disable(logging.CRITICAL)
+
+
+def run_tests(*test_args):
+    from django_nose import NoseTestSuiteRunner
+    test_runner = NoseTestSuiteRunner()
+    if not test_args:
+        test_args = ['tests']
+    num_failures = test_runner.run_tests(test_args)
+    if num_failures:
+        sys.exit(num_failures)
+
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    options, args = parser.parse_args()
+    run_tests(*args)
--- tests/stores_tests.py
+++ tests/stores_tests.py
@@ -1,24 +1,24 @@
 from django.test import TestCase
 
-from django_dynamic_fixture import get
+from django_dynamic_fixture import get as G
 
 from stores.models import Store
 
 
-class StoreTests(TestCase):
+class TestStore(TestCase):
 
     def test_getting_shipping_data(self):
-        store = get(Store)
+        store = G(Store)
         self.assertItemsEqual(store.get_shipping_data(), [
             'line1', 'line2', 'line3', 'line4',
             'postcode', 'country', 'state'
         ])
 
-    def test_pickup_stores_query(self):
-        store1 = get(Store, is_pickup_store=True)
-        store2 = get(Store, is_pickup_store=True)
-        get(Store, is_pickup_store=False)
-        store4 = get(Store, is_pickup_store=True)
+    def test_querying_available_pickup_stores(self):
+        store1 = G(Store, is_pickup_store=True)
+        store2 = G(Store, is_pickup_store=True)
+        G(Store, is_pickup_store=False)
+        store4 = G(Store, is_pickup_store=True)
 
         stores = Store.objects.pickup_stores()
         self.assertItemsEqual(
