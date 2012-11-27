262bfb89311b51ce2de74271c4717282d53cedc6
Sebastian Vetter <sebastian@roadside-developer.com>
2012-11-28 10:07:18 +1100

    Switch order of store URLs

--- sandbox/sandbox/urls.py
+++ sandbox/sandbox/urls.py
@@ -11,9 +11,9 @@ from stores.dashboard.app import application as dashboard_app
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'', include(shop.urls)),
     url(r'^dashboard/stores/', include(dashboard_app.urls)),
     url(r'^stores/', include(stores_app.urls)),
+    url(r'^', include(shop.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
 
