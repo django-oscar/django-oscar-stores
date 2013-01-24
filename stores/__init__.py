ebac3deeb7794417bea6a817c1ccb56658d0543c
David Winterbottom <david.winterbottom@gmail.com>
2013-01-24 13:55:55 +0000

    Add TEMPLATE_DIR setting

--- stores/__init__.py
+++ stores/__init__.py
@@ -1,1 +1,11 @@
+import os
+
+
 __version__ = (0, 2, 0, 'dev', 0)
+
+
+# Keep a setting for the path to the templates in case a project subclasses the
+# models and still needs to use the templates
+TEMPLATE_DIR = os.path.join(
+    os.path.dirname(__file__),
+    'templates')
