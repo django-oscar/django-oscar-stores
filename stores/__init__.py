358dd1867d46c761a8762b7c06a160c825c7324b
Samir Shah <solaris.smoke@gmail.com>
2019-08-12 08:57:54 +0300

    Remove redundant TEMPLATE_DIR variable.

--- stores/__init__.py
+++ stores/__init__.py
@@ -1,9 +1,1 @@
-import os
-
 default_app_config = 'stores.apps.StoresConfig'
-
-# Keep a setting for the path to the templates in case a project subclasses the
-# models and still needs to use the templates
-TEMPLATE_DIR = os.path.join(
-    os.path.dirname(__file__),
-    'templates')
